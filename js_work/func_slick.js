9095adb970666d013abe97d3bbc562819667e090
nakass35 <88621046+nakass35@users.noreply.github.com>
2021-08-21 16:23:01 +0900

    Add files via upload

---
+++ js_work/func_slick.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.slider').slick({
+    dots:true, //ページャーを表示（スライダー下の黒い丸）
+    centerMode: true, //要素を中央寄せにする
+    centerPadding:'20%', //両サイドの見えている部分のサイズ
+    autoplay:true, //自動で画像を切り替える
+    autoplaySpeed:3000, }
+  ); //スライダーにする部分のclassまたはidを指定
+});
